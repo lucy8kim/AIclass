b1fc5db29a1763ffaa9e76b7e19d773b7cd55af0
lucy8kim <162088084+lucy8kim@users.noreply.github.com>
2024-03-07 15:53:21 +0900

    Add files via upload

---
+++ 새 폴더/calcluate.py
@@ -0,0 +1,17 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> x = 100
+>>> y = 200
+>>> x + y
+300
+>>> x - y
+-100
+>>> x * y
+20000
+>>> x / y
+0.5
+>>> x ** 2
+10000
+>>> n = 5
+>>> n ** 2
+25

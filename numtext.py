90eaebca5a1cd95bbb7887762b8600ab52bf86ce
lucy8kim <162088084+lucy8kim@users.noreply.github.com>
2024-03-07 19:19:04 +0900

    Add files via upload

---
+++ numtext.py
@@ -0,0 +1,26 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hello World!')
+Hello World!
+>>> print('Helio', 'World!')
+Helio World!
+>>> print('Hello'+'World!')
+HelloWorld!
+>>> print(100)
+100
+>>> print(2+4)
+6
+>>> 100
+100
+>>> 10+20
+30
+>>> 10-20
+-10
+>>> 10*20
+200
+>>> 10/20
+0.5
+>>> 123*8765
+1078095
+>>> 10+20*30
+610

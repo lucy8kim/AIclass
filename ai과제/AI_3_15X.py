e1538e443a5704dbf6ae61b8cc6c154c0ec229bb
lucy8kim <162088084+lucy8kim@users.noreply.github.com>
2024-03-19 12:22:44 +0900

    Add files via upload

---
+++ ai과제/AI_3_15X.py
@@ -0,0 +1,4 @@
+for i in range(1 ,10):
+    a = 2*i
+
+    print('2 * {} = {}'.format(i, a))
---
+++ ai과제/AI_3_15Y.py
@@ -0,0 +1,7 @@
+i = 1
+while i < 10:
+    s = 2*i
+    
+
+    print('2 * {} = {}'.format(i, s))
+    i += 1
